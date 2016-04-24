345efd98be4be0c966b636d6bf81787b3535ab49
fghh dfa <fghh dfa>
2016-04-24 19:24:36 +0300

    Initial BoostrapJs

--- RSS to Hybrid News App/app/app.component.js
+++ RSS to Hybrid News App/app/app.component.js
@@ -2,7 +2,7 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>My First Angular 2 App</h1>'
+      template: '<div class="container-fluid">container</div><h1>My First Angular 2 App<button type="button" class="btn btn-primary btn-sm">Test</button></h1>'
     })
     .Class({
       constructor: function() {}
