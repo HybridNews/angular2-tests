e575ba43191a2c163bc7a994489749b91777428d
fghh dfa <fghh dfa>
2016-04-29 01:29:29 +0300

    wip

--- RSS to Hybrid News App/app/app.js
+++ RSS to Hybrid News App/app/app.js
@@ -1,5 +1,14 @@
-(function(app) {
-  document.addEventListener('DOMContentLoaded', function() {
-    ng.platform.browser.bootstrap(app.AppComponent);
-  });
+(function (app) {
+    app.HybridNewsComponent = ng.core.Component({
+         selector: 'hybrid-news',
+         directives: [app.AppComponent],
+         template: '<articles-list></articles-list>'
+     })
+     .Class({
+         constructor: function () { }
+     });
+
+    document.addEventListener('DOMContentLoaded', function () {
+        ng.platform.browser.bootstrap(app.HybridNewsComponent);
+    });
 })(window.app || (window.app = {}));
--- RSS to Hybrid News App/app/components/articles-list/articles-list.component.js
+++ RSS to Hybrid News App/app/components/articles-list/articles-list.component.js
@@ -5,6 +5,13 @@
           templateUrl: 'app/components/articles-list/articles-list.template.html'
       })
       .Class({
-          constructor: function () { }
+          constructor: function () {
+              this.articles = [
+                  { title: "Title1", description: "Some random text 1" },
+                  { title: "Title2", description: "Some random text 2" },
+                  { title: "Title3", description: "Some random text 3" },
+                  { title: "Title4", description: "Some random text 4" },
+              ];
+          }
       });
 })(window.app || (window.app = {}));
