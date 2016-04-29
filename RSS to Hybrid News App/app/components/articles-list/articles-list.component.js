903b7892b26c1ee960a161a54b24d8df2e966325
fghh dfa <fghh dfa>
2016-04-30 02:28:24 +0300

    Added Corousel!

--- RSS to Hybrid News App/app/components/articles-list/articles-list.component.js
+++ RSS to Hybrid News App/app/components/articles-list/articles-list.component.js
@@ -1,26 +1,32 @@
 import {Component} from 'angular2/core';
+import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
+import {Carousel} from 'ng2-bootstrap/components/carousel/carousel.component';
+import {Slide} from 'ng2-bootstrap/components/carousel/slide.component';
 
 (function (app) {
-    app.ArticlesListComponent =
-      Component({
-          selector: 'articles-list',
-          templateUrl: 'app/components/articles-list/articles-list.template.html'
-      })
-      .Class({
-          constructor: [app.articlesService, app.xmlToJsonService, function ArticlesListComponent(articlesService, xmlToJsonService) {
-              var that = this;
-              articlesService.getArticles()
-                  .subscribe(
-                      function (response) {
-                          var result = xmlToJsonService.getJson(response.text()).channel.item;
-                          that.articles = result;
-                      });
-              this.articles = [
-                  { title: "Title1", description: "Some random text 1" },
-                  { title: "Title2", description: "Some random text 2" },
-                  { title: "Title3", description: "Some random text 3" },
-                  { title: "Title4", description: "Some random text 4" }
-              ];
-          }]
-      });
+	app.ArticlesListComponent =
+	  Component({
+	  	selector: 'articles-list',
+	  	directives: [Carousel, Slide, CORE_DIRECTIVES, FORM_DIRECTIVES],
+		templateUrl: 'app/components/articles-list/articles-list.template.html'
+	  })
+	  .Class({
+		constructor: [app.articlesService, app.xmlToJsonService, function ArticlesListComponent(articlesService, xmlToJsonService) {
+			var that = this;
+			that.baseUrl = "";
+			that.articles = [];
+			that.myInterval = 2000;
+			that.noWrapSlides = false;
+			that.slides = [];
+			articlesService.getArticles().subscribe(
+				function (response) {
+					var channel = xmlToJsonService.getJson(response.text()).channel;
+					that.baseUrl = channel.link;
+					var items = channel.item;
+					that.articles = items;
+					that.slides = items.slice(0, 4);
+				}
+			);
+		}]
+	  });
 })(window.app || (window.app = {}));
